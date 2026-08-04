8e733419a633508925af2145240d2d0212e2151a
Ankit Pal <ankitpal242804@gmail.com>
2026-08-04 10:22:12 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,5 +1,5 @@
 def print_pyramid(rows):
-    for i in range(1, rows + 1):
+for i in range(1, rows + 1):
         # Print leading spaces
         print(" " * (rows - i), end="")
         # Print asterisks with a space in between
